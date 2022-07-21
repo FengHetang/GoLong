6d52aceb7b8988936d5563a38e4ccd9502bab75c
戈亓 <1994975650@qq.com>
2022-07-21 18:56:04 +0800

    100 demo 02

---
+++ 100demo/03/main.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: 戈亓
+ * @Description:
+ * @File:  main
+ * @Version: 1.0.0
+ * @Date: 2022/7/21 16:19
+ */
+
+// 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+
+package main
+
+func main() {
+
+}
